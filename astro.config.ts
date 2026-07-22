e53008c0b1fe7cd298b6f6e53396ecd0a21f2de0
RiverOnVenus <RiverOnVenus@gmail.com>
2026-07-22 21:12:01 +0800

    chore: set site language to Chinese

--- astro.config.ts
+++ astro.config.ts
@@ -29,8 +29,8 @@ export default defineConfig({
     }),
   ],
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ["zh-CN"],
+    defaultLocale: "zh-CN",
     routing: {
       prefixDefaultLocale: false,
     },
